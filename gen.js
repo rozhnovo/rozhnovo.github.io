578462f741ee6071168fc2d9bcf2089ffb3c6c19
Maxim <maxim@prudnikov.su>
2019-10-11 14:26:34 +0300

    edit

--- gen.js
+++ gen.js
@@ -1,12 +1,13 @@
 var fs=require("fs");
 var uuidv4 = require('uuid/v4');
 var path=require("path");
+var ids=require("short-id");
 
 var source_name="index";
 var dir="tropa_2019";
 var num_files=16;
 for( i =0; i<num_files;i++){
-    var fn=uuidv4();
+    var fn=ids.generate();
     console.log(fn);
     src_fn=path.join(dir,source_name+".html");
     dst_fn=path.join(dir,""+i+"_"+fn+".html");
