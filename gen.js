9398e0cb1c913ecfe04277105f459ad258b3b341
Maxim <maxim@prudnikov.su>
2019-10-11 14:43:22 +0300

    add

--- gen.js
+++ gen.js
@@ -2,11 +2,15 @@ var fs=require("fs");
 var uuidv4 = require('uuid/v4');
 var path=require("path");
 var ids=require("short-id");
+var moment=require("moment");
 
 var source_name="_";
 var dir="tropa_2019";
 var num_files=16;
-for( i =1; i<=num_files;i++){
+var site_prefix="https://rozhnovo.github.io/tropa_2019/";
+var now=moment().format("YYYYMMDDhhmmss");
+
+for( i =0; i<num_files;i++){
     var fn=ids.generate();
     console.log(fn);
     src_fn=path.join(dir,source_name+".html");
